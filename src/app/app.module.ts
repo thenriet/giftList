dfb87a7c3d7af965ce796f2213ea5b0dd6c95544
Marina <masset.marina@live.fr>
2023-01-30 15:12:33 +0100

    update css cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardListComponent } from './card-list/card-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CardListComponent
   ],
   imports: [
     BrowserModule,
